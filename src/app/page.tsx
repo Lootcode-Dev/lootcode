7e2675cc9b3a97931191574e709cc7147666eaea
Dylan Vidal <dylanvidal1204@gmail.com>
2024-03-01 01:12:21 -0500

    Tests Discord Github Webhook Interactions 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
           ) : (
             <div className="mt-4"> Not signed in </div>
           )}
-          <div>Webhook Testing</div>
+          <div>Webhook Testing 2</div>
         </div>
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
