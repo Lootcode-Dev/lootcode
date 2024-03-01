82fd1208f4d027cb2b8408968930c3b827901a25
Dylan Vidal <dylanvidal1204@gmail.com>
2024-03-01 00:03:43 -0500

    Initializing Commit
    Sets up tRPC, Clerk Auth, PlanetScale, SQL Database, and Middleware for Protected Routes

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import "~/styles/globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import { Inter } from "next/font/google";
+
+import { TRPCReactProvider } from "~/trpc/react";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "Lootcode",
+  description:
+    "A fantasy themed code runner for learning Data Structures and Algorithms.",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html lang="en">
+        <body className={`font-sans ${inter.variable}`}>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
