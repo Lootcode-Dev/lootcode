0d30ebc4d9654d243c5d9a59ca463e259b560c81
Liverneck <75819270+Liverneck@users.noreply.github.com>
2024-03-09 13:53:40 -0500

    Add header/region description for map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -12,6 +12,15 @@ import ReactMarkdown from "react-markdown";
 import { Link } from "lucide-react";
 import NodeGraph from "~/components/nodegraph";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog"
+
 interface Node {
   pos: number[];
   name: string;
@@ -29,9 +38,20 @@ export default function Page() {
     { enabled: false, retry: false },
   );
 
+  const {data: desc, refetch: getChDesc } = api.map.getDescription.useQuery(
+    {
+      name: nameToFileName(mapFile.chapters[chapter]?.name ?? "failure"),
+    },
+    { enabled: false, retry: false },
+  );
+
   useEffect(() => {
     getProblem();
   }, [chapter, selNode]);
+
+  useEffect(() => {
+    getChDesc();
+  }, [chapter])
 
   if (chapter != -1 && !mapFile.chapters[0])
     return (
@@ -46,48 +66,75 @@ export default function Page() {
     <main className="z-10 flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="flex size-full items-center justify-center p-4">
         {chapter != -1 ? (
-          <div className="flex h-[80vh] w-full justify-center">
-            <div className="items-top mx-2 flex flex-col rounded-xl bg-[#15162c] p-4">
-              <Button
-                className="mt-2 bg-purple-700"
-                onClick={() => {
-                  setSelNode(-1);
-                  setChapter(-1);
-                  console.log(selNode);
-                }}
-              >
-                Back
-              </Button>
-            </div>
-            <NodeGraph
-              nodes={mapFile.chapters[chapter]?.nodes}
-              nodeRadius={25}
-              getNode={selNode}
-              setNode={setSelNode}
-            />
+            <div className="grow">
+            
+              <div className="grid justify-items-center">
+                <Dialog>
+                  <DialogTrigger>
+                    <Button className="mb-4 bg-purple-700 text-2xl text-center font-bold">{mapFile.chapters[chapter]?.name}</Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>{mapFile.chapters[chapter]?.name}</DialogTitle>
+                      <DialogDescription>
+                      <ReactMarkdown
+                        remarkPlugins={[remarkGfm]}
+                        className="prose grow overflow-auto scroll-smooth 
+                        rounded-xl p-4"
+                      >
+                        {desc}
+                      </ReactMarkdown>
+                      </DialogDescription>
+                    </DialogHeader>
+                  </DialogContent>
+                </Dialog>
+              </div>
 
-            <div className="flex w-[20vw] flex-col">
-              <ReactMarkdown
-                remarkPlugins={[remarkGfm]}
-                className="prose grow overflow-auto scroll-smooth 
-                rounded-xl bg-[#15162c] p-4 text-white prose-headings:text-purple-500 prose-em:text-yellow-200"
-              >
-                {selNode != -1
-                  ? problem
-                  : "# Welcome to Lootcode!\n*Select a node to begin your adventure...*"}
-              </ReactMarkdown>
+              <div className="flex h-[75vh] w-full justify-center">
+              
+                <div className="items-top mx-2 flex flex-col rounded-xl bg-[#15162c] p-4">
+                  <Button
+                    className="mt-2 bg-purple-700"
+                    onClick={() => {
+                      setSelNode(-1);
+                      setChapter(-1);
+                      console.log(selNode);
+                    }}
+                  >
+                    Back
+                  </Button>
+                </div>
+                <NodeGraph
+                  nodes={mapFile.chapters[chapter]?.nodes}
+                  nodeRadius={25}
+                  getNode={selNode}
+                  setNode={setSelNode}
+                />
 
-              {selNode != -1 && problem != undefined ? (
-                <a
-                  href={"/map/" + nameToFileName(getNodeName(chapter, selNode))}
-                >
-                  <Button className="mt-2 w-full bg-purple-700">Embark</Button>
-                </a>
-              ) : (
-                <div />
-              )}
+                <div className="flex w-[20vw] flex-col">
+                  <ReactMarkdown
+                    remarkPlugins={[remarkGfm]}
+                    className="prose grow overflow-auto scroll-smooth 
+                    rounded-xl bg-[#15162c] p-4 text-white prose-headings:text-purple-500 prose-em:text-yellow-200"
+                  >
+                    {selNode != -1
+                      ? problem
+                      : desc}
+                  </ReactMarkdown>
+
+                  {selNode != -1 && problem != undefined ? (
+                    <a
+                      href={"/map/" + nameToFileName(getNodeName(chapter, selNode))}
+                    >
+                      <Button className="mt-2 w-full bg-purple-700">Embark</Button>
+                    </a>
+                  ) : (
+                    <div />
+                  )}
+                </div>
+              </div>
+              
             </div>
-          </div>
         ) : (
           <div className="flex h-[80vh] w-full justify-center">
             <NodeGraph
@@ -115,5 +162,5 @@ function getNodeName(ch: number, i: number): string {
 }
 
 function nameToFileName(name: string): string {
-  return name.split(" ").join("_").toLowerCase();
+  return name.split(" ").join("-").toLowerCase();
 }
---
+++ src/descriptions/chapter-1.md
@@ -0,0 +1,3 @@
+# Test file
+This is a test of loading a description.
+Womp womp.
