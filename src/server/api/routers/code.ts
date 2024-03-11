d67be1943434e10626d3e4bb38eae0abc534366f
MelonFruit7 <leogofman23@gmail.com>
2024-03-10 22:07:15 -0400

    Code Grade Server 80%, Made Dynamic

--- src/server/api/routers/code.ts
+++ src/server/api/routers/code.ts
@@ -4,10 +4,11 @@
 import { writeFile } from "fs/promises";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
-import { $, ProcessOutput } from "zx";
+import { $ } from "zx";
 import { readdirSync } from "fs";
 import { readFile } from "fs/promises";
 import { TRPCError } from "@trpc/server";
+
 
 export const codeRouter = createTRPCRouter({
   getProblem: protectedProcedure
@@ -20,6 +21,7 @@ export const codeRouter = createTRPCRouter({
 
       return contents;
     }),
+
   runProblem: protectedProcedure
     .input(z.object({ name: z.string(), code: z.string(), lang: z.string() }))
     .query(async ({ input, ctx }) => {
@@ -27,285 +29,124 @@ export const codeRouter = createTRPCRouter({
       $.verbose = false;
 
       // Prepare the output
-      interface caseRes {
-        num: number;
-        expected: string;
-        output: string;
-        result: boolean;
-        runtimeError?: string;
-      }
-      interface codeGradeResult {
-        compileError?: string;
-        numPassed: number;
-        numFailed: number;
-        cases: caseRes[];
-      }
+      interface caseRes {num: number; expected: string; output: string; result: boolean; runtimeError?: string} //Case Interface for cases in teh Code Grade Interface
+      interface codeGradeResult {compileError?: string; numPassed: number; numFailed: number; cases: caseRes[]} //Code Grade Interface
 
-      const codeGradeResponse: codeGradeResult = {
-        numPassed: 0,
-        numFailed: 0,
-        cases: [],
-      };
+      const codeGradeResponse: codeGradeResult = {numPassed: 0, numFailed: 0, cases: []}; //OUR RESPONSE BACK TO OUR CLINET
 
-      // Log the request
-      console.log("Getting problem description");
+
+      interface langType {ext: string; compile ?: string; run : string} //Our Interface for our dictionary 
+      const codePath = `./temp/${ctx.userId}${input.name}`; //The path is based on the users id and problem name
+      const problemPathInput = `./src/problems/${input.name}/input/` //The path is based on the problem name
+
+      const langSearch: Record<string, langType> = {
+        "python": {ext: "py", run: `python3 ${codePath}.py`},
+        "java": {ext: "java", compile: `javac ${codePath}.java`, run: `java -classpath ./temp/ ${ctx.userId}${input.name}`},
+        "c": {ext: "c", compile: `gcc ${codePath}.c -o ${codePath}.out -lm`, run: `${codePath}.out`},
+        "cpp": {ext: "cpp", compile: `g++ ${codePath}.cpp -o ${codePath}.out -lm`, run: `${codePath}.out`}
+      }
+      const langObject = langSearch[input.lang] ?? "Error"; //We have the appropriate necessities for our language stored in this object
+      if (langObject == "Error") {
+        console.log("Invalid language");
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Invalid language",
+        });
+      }
 
       // Get all of our input files
-      const filenames = readdirSync(`./src/problems/${input.name}/input`);
+      const filenames = readdirSync(`${problemPathInput}`);
       console.log(filenames);
 
-      switch (input.lang) {
-        case "python":
-          console.log("Python detected");
+      // Write the code to a temp file with the correct extension
+      await writeFile(`${codePath}.${langObject.ext}`, input.code);
 
-          // Write the code to a temp file with the correct extension
-          await writeFile(`./temp/${ctx.userId}${input.name}.py`, input.code);
+      if (langObject.compile) {
+        try {
+          await $withoutEscaping`${langObject.compile}` //Compile our code
+          console.log("Compiled Successfully");
+        } catch(error) {
+          codeGradeResponse.compileError = "Compile Time Error";
+          console.log(error);
 
-          // Iterate through the input files
-          let i = 1;
-          for (const file of filenames) {
-            // Declare our case object
-            const thisCase: caseRes = {
-              num: i,
-              expected: "",
-              output: "",
-              result: false,
-              runtimeError: "",
-            };
-
-            // Run the code with the input and write the output to a temp file
-            try {
-              await $`timeout 1s  python3 ./temp/${ctx.userId}${input.name}.py < ./src/problems/${input.name}/input/${file} > ./temp/${ctx.userId}${input.name}.txt`;
-            } catch (error: any) {
-              if (error.exitCode === 124) {
-                //TLE exist code is 124
-                console.log("Time limit exceeded");
-                thisCase.runtimeError = "Time limit exceeded";
-                thisCase.output = "Time limit exceeded";
-                const expectedFile = file.replace(".in", ".out");
-                const expected =
-                  await $`cat ./src/problems/${input.name}/output/${expectedFile}`;
-                thisCase.expected = expected.stdout;
-                codeGradeResponse.cases.push(thisCase);
-                continue;
-              }
-              console.log("Runtime error");
-              console.log(error.stderr);
-              thisCase.runtimeError = error.stderr;
-              const expectedFile = file.replace(".in", ".out");
-              const expected =
-                await $`cat ./src/problems/${input.name}/output/${expectedFile}`;
-              thisCase.expected = expected.stdout;
-              thisCase.output = "Runtime error";
-              codeGradeResponse.cases.push(thisCase);
-              continue;
-            }
-
-            // Test the output against the expected output
-            const expectedFile = file.replace(".in", ".out");
-            const output = await $`cat ./temp/${ctx.userId}${input.name}.txt`;
-            const expected =
-              await $`cat ./src/problems/${input.name}/output/${expectedFile}`;
-            console.log("Output: " + output.stdout);
-            console.log("Expected: " + expected.stdout);
-            thisCase.expected = expected.stdout;
-            thisCase.output = output.stdout;
-            if (output.stdout === expected.stdout) {
-              console.log("Correct answer\n");
-              thisCase.result = true;
-              codeGradeResponse.numPassed++;
-            } else {
-              console.log("Wrong answer\n");
-              codeGradeResponse.numFailed++;
-            }
-            codeGradeResponse.cases.push(thisCase);
-            i++;
-          }
-
-          // Cleanup
-          await $`rm ./temp/${ctx.userId}${input.name}.py`;
-          await $`rm ./temp/${ctx.userId}${input.name}.txt`;
-
-          break;
-
-        case "java":
-          console.log("Java detected");
-
-          //Write the code to a temp file with the correct extension
-          await writeFile(`./temp/${ctx.userId}${input.name}.java`, input.code);
-          try {
-            await $`javac ./temp/${ctx.userId}${input.name}.java`;
-            console.log("Compiled Successfully");
-          } catch (error: any) {
-            console.log("COMPILATION ERROR DETECTED");
-            console.log(error.stderr);
-          }
-
-          for (const file of filenames) {
-            await $`java ./temp/${ctx.userId}${input.name} < ./src/problems/${input.name}/input/${file} > ./temp/${ctx.userId}${input.name}.txt`;
-
-            const expectedFile = file.replace(".in", ".out");
-            const output = await $`cat ./temp/${ctx.userId}${input.name}.txt`;
-            const expected =
-              await $`cat ./src/problems/${input.name}/output/${expectedFile}`;
-            console.log("Output: " + output.stdout);
-            console.log("Expected: " + expected.stdout);
-            if (output.stdout === expected.stdout) {
-              console.log("Correct answer\n");
-            } else {
-              console.log("Wrong answer\n");
-            }
-          }
-
-          // Cleanup
-          await $`rm ./temp/${ctx.userId}${input.name}.java`;
-          await $`rm ./temp/${ctx.userId}${input.name}.class`;
-          await $`rm ./temp/${ctx.userId}${input.name}.txt`;
-
-          break;
-
-        case "c":
-          console.log("C detected");
-
-          // Write the code to a temp file with the correct extension, and compile
-          await writeFile(`./temp/${ctx.userId}${input.name}.c`, input.code);
-          try {
-            await $`gcc ./temp/${ctx.userId}${input.name}.c -o ./temp/${ctx.userId}${input.name}.out`;
-            console.log("Compiled Successfully");
-          } catch (error: any) {
-            console.log("COMPILATION ERROR DETECTED");
-            console.log(error.stderr);
-          }
-
-          // Iterate through the input files
-          for (const file of filenames) {
-            // Run the code with the input and write the output to a temp file
-            await $`./temp/${ctx.userId}${input.name}.out < ./src/problems/${input.name}/input/${file} > ./temp/${ctx.userId}${input.name}.txt`;
-
-            // Test the output against the expected output
-            const expectedFile = file.replace(".in", ".out");
-            const output = await $`cat ./temp/${ctx.userId}${input.name}.txt`;
-            const expected =
-              await $`cat ./src/problems/${input.name}/output/${expectedFile}`;
-            console.log("Output: " + output.stdout);
-            console.log("Expected: " + expected.stdout);
-            if (output.stdout === expected.stdout) {
-              console.log("Correct answer\n");
-            } else {
-              console.log("Wrong answer\n");
-            }
-          }
-
-          // Cleanup
-          await $`rm ./temp/${ctx.userId}${input.name}.c`;
-          await $`rm ./temp/${ctx.userId}${input.name}.out`;
-          await $`rm ./temp/${ctx.userId}${input.name}.txt`;
-
-          break;
-
-        case "cpp":
-          console.log("C++ detected");
-
-          //Write the code to a temp file with the correct extension
-          await writeFile(`./temp/${ctx.userId}${input.name}.cpp`, input.code);
-          try {
-            await $`g++ ./temp/${ctx.userId}${input.name}.cpp -o ./temp/${ctx.userId}${input.name}.out`;
-            console.log("Compiled Successfully");
-          } catch (error: any) {
-            console.log("COMPILATION ERROR DETECTED");
-            console.log(error.stderr);
-          }
-
-          for (const file of filenames) {
-            // Time limit
-            try {
-              await $`timeout 1s ./temp/${ctx.userId}${input.name}.out < ./src/problems/${input.name}/input/${file} > ./temp/${ctx.userId}${input.name}.txt`;
-            } catch (error: any) {
-              if (error.exitCode === 124) {
-                //TLE exist code is 124
-                console.log("Time limit exceeded");
-              } else {
-                console.log("Runtime error");
-              }
-            }
-
-            const expectedFile = file.replace(".in", ".out");
-            const output = await $`cat ./temp/${ctx.userId}${input.name}.txt`;
-            const expected =
-              await $`cat ./src/problems/${input.name}/output/${expectedFile}`;
-            console.log("Output: " + output.stdout);
-            console.log("Expected: " + expected.stdout);
-            if (output.stdout === expected.stdout) {
-              console.log("Correct answer\n");
-            } else {
-              console.log("Wrong answer\n");
-            }
-          }
-
-          // Cleanup
-          await $`rm ./temp/${ctx.userId}${input.name}.cpp`;
-          await $`rm ./temp/${ctx.userId}${input.name}.out`;
-          await $`rm ./temp/${ctx.userId}${input.name}.txt`;
-
-          break;
-        default:
-          console.log("Invalid language");
-          throw new TRPCError({
-            code: "BAD_REQUEST",
-            message: "Invalid language",
-          });
+          await $withoutEscaping`${codePath}.${langObject.ext}`; //Clean Up
+          return codeGradeResponse; //Our code didn't compile no need to test the cases 
+        }
       }
+
+      // Iterate through the input files
+      let i = 1;
+      for (const file of filenames) {
+        // Declare our case object
+        const thisCase: caseRes = {
+          num: i, //Case Number
+          expected: "", //Expected Result
+          output: "", //Our program output
+          result: false, //Did we pass all cases?
+          runtimeError: "", //We had an error during runtime
+        };
+
+        // Run the code with the input and write the output to a temp file
+        try {
+          const IO = `< ${problemPathInput}${file} > ${codePath}.txt`;
+          await $withoutEscaping`timeout 1s ${langObject.run} ${IO}`;
+        } catch (error: any) { //Error with running the code
+          if (error.exitCode === 124) { //Time Limit Exceeded
+            //TLE exist code is 124
+            console.log("Time limit exceeded");
+            thisCase.runtimeError = "Time limit exceeded";
+            thisCase.output = "Time limit exceeded";
+          } else { //Runtime error
+            console.log("Runtime error");
+            console.log(error.stderr);
+            thisCase.runtimeError = error.stderr;
+            thisCase.output = "Runtime error";
+          }
+
+          //Get the expected output
+          const expectedFile = file.replace(".in", ".out");
+          const expected = await $`cat ./src/problems/${input.name}/output/${expectedFile}`;
+          thisCase.expected = expected.stdout;
+          
+          codeGradeResponse.cases.push(thisCase);
+          continue;
+        }
+
+        // Test the output against the expected output
+        const expectedFile = file.replace(".in", ".out");
+        const output = await $`cat ${codePath}.txt`;
+        const expected = await $`cat ./src/problems/${input.name}/output/${expectedFile}`;
+
+        console.log("Output: " + output.stdout);
+        console.log("Expected: " + expected.stdout);
+        thisCase.expected = expected.stdout;
+        thisCase.output = output.stdout;
+        if (output.stdout === expected.stdout) {
+          console.log("Correct answer\n");
+          thisCase.result = true;
+          codeGradeResponse.numPassed++;
+        } else {
+          console.log("Wrong answer\n");
+          codeGradeResponse.numFailed++;
+        }
+        codeGradeResponse.cases.push(thisCase);
+        i++;
+      }
+
+      // Cleanup
+      await $`rm ${codePath}.*`; //Remove all the user files in temp
 
       console.log(codeGradeResponse);
       return codeGradeResponse;
     }),
-  testTemplate: protectedProcedure
-    .input(z.object({ name: z.string(), code: z.string(), lang: z.string() }))
-    .query(async ({ input, ctx }) => {
-      if (input.lang != "c") {
-        console.log("THIS TEST ONLY WORKS WITH C");
-        return 0;
-      }
-      $.verbose = false;
-
-      //const filenames = readdirSync(`./src/problems/${input.name}/input`);
-
-      // Write the code to a temp file with the correct extension, and compile
-      await writeFile(`./temp/${ctx.userId}${input.name}.c`, input.code);
-      try {
-        const command = `gcc ./temp/${ctx.userId}${input.name}.c -o ./temp/${ctx.userId}${input.name}.out`;
-        await $(command);
-        console.log("Compiled Successfully TEMPLATE TEST");
-      } catch (error: any) {
-        console.log("COMPILATION ERROR DETECTED TEMPLATE TEST");
-        console.log(error.stderr);
-      }
-
-      /*
-        // Iterate through the input files
-        for (const file of filenames) {
-          // Run the code with the input and write the output to a temp file
-          await $`./temp/${ctx.userId}${input.name}.out < ./src/problems/${input.name}/input/${file} > ./temp/${ctx.userId}${input.name}.txt`;
-
-          // Test the output against the expected output
-          const expectedFile = file.replace(".in", ".out");
-          const output = await $`cat ./temp/${ctx.userId}${input.name}.txt`;
-          const expected =
-            await $`cat ./src/problems/${input.name}/output/${expectedFile}`;
-          console.log("Output: " + output.stdout);
-          console.log("Expected: " + expected.stdout);
-          if (output.stdout === expected.stdout) {
-            console.log("Correct answer\n");
-          } else {
-            console.log("Wrong answer\n");
-          }
-        }
-
-        // Cleanup
-        await $`rm ./temp/${ctx.userId}${input.name}.c`;
-        await $`rm ./temp/${ctx.userId}${input.name}.out`;
-        await $`rm ./temp/${ctx.userId}${input.name}.txt`;
-        */
-      return 0;
-    }),
 });
+
+export function $withoutEscaping(pieces: TemplateStringsArray, ...args: unknown[]): any {
+  const origQuote = $.quote //ZX shell escapes using this quote function
+  try {
+      $.quote = unescapedCmd => unescapedCmd //Change the quote function to not esacape
+      return $(pieces, args) //Return the result of our shell command without escaping
+  } finally {
+      $.quote = origQuote //Set the functionality of quote back to being able to escape (without changing our return, so our return returned an unescaped sequence fixing our compilation problems)
+  }
+}
--- temp/user_2dVgHUO3NNHlixBjjwiyop8puTFtrial.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  int num1, num2;
-  scanf("%d %d", &num1, &num2);
-  printf("%d", num1 + num2);
-  return 0;
-}
