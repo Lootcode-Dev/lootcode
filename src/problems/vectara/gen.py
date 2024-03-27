9c0ca16e43102aa03c1f2582c4cbd9023687d091
Dylan Vidal <dylanvidal1204@gmail.com>
2024-03-26 20:38:19 -0400

    Update gen.py

--- src/problems/vectara/gen.py
+++ src/problems/vectara/gen.py
@@ -9,13 +9,8 @@ def generate_random_numbers(n, x, y):
 n = 100  # Number of random numbers to generate
 x = 0   # Lower bound
 y = 1 # Upper bound
-z = 50 # Segment size
-# j = 1   # Lower bound for segment size
-# k = 10  # Upper bound for segment size
-# z = random.randint(j, k)
 
 random_numbers = generate_random_numbers(n, x, y)
 numbers_string = ' '.join(map(str, random_numbers))
 
-print(n, z)
 print(numbers_string, end='')
