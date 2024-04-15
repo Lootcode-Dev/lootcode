7350969b32b31ffdf055d4fd6b4e52ab28e3eeda
Dylan Vidal <dylanvidal1204@gmail.com>
2024-04-15 15:01:19 -0400

    Adds a test gameloop that actually works

--- src/components/testgame.tsx
+++ src/components/testgame.tsx
@@ -1,6 +1,64 @@
+"use client";
+import { useEffect, useState } from "react";
+import { api } from "~/trpc/react";
+import { Button } from "./ui/button";
+
+interface Enemy {
+  image: string;
+  name: string;
+  health: number;
+  attackSpeed: number;
+  critChance: number;
+  strength: number;
+  armor: number;
+  magic: number;
+  resist: number;
+}
+
 export default function Testgame() {
+  const [enemies, setEnemies] = useState<Enemy[] | null>(null);
+
+  const { data } = api.game.getEncounter.useQuery({
+    encounterid: "test",
+  });
+
+  useEffect(() => {
+    setEnemies(data);
+  }, [data]);
+
+  function gameLoop() {
+    if (enemies) {
+      const interval = setInterval(() => {
+        setEnemies((prevEnemies) => {
+          if (prevEnemies) {
+            const updatedEnemies = prevEnemies.map((enemy) => ({
+              ...enemy,
+              health: enemy.health - 5,
+            }));
+
+            const allEnemiesDead = updatedEnemies.every(
+              (enemy) => enemy.health <= 0,
+            );
+            if (allEnemiesDead) {
+              clearInterval(interval);
+            }
+
+            return updatedEnemies;
+          }
+
+          return prevEnemies;
+        });
+      }, 1000);
+    }
+  }
+
+  useEffect(() => {
+    console.log(enemies);
+  }, [enemies]);
+
   return (
     <div className="grid h-[92.5vh] grid-cols-2 bg-[#282A36] text-white">
+      <Button onClick={gameLoop}>Start</Button>
       <div className="flex items-center justify-center">
         <div className="flex flex-col items-center space-y-12">
           <div>Player</div>
@@ -10,6 +68,14 @@ export default function Testgame() {
         <div className="flex flex-col items-center space-y-12">
           <div>Enemy</div>
           <div>Enemy 2</div>
+          {enemies?.map((enemy, index) => (
+            <div key={index}>
+              <div>{enemy.name}</div>
+              <div>{enemy.health}</div>
+              <div>{enemy.health}</div>
+              <div>{enemy.armor}</div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
